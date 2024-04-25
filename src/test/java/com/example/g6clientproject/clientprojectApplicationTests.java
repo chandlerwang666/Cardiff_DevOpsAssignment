8d7b3af185c419e91a8ea655ef28d23fcd863470
c23087264 <wangq79@cardiff.ac.uk>
2024-04-25 18:17:03 +0100

    add applicationtest

---
+++ src/test/java/com/example/g6clientproject/clientprojectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.g6clientproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class clientprojectApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
