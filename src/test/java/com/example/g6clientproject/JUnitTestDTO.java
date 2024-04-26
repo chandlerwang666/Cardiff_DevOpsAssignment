1ca319e48fb76f437b6d0c7c7c775b889cb0e416
Qiming <wangq79@cardiff.ac.uk>
2024-04-26 02:33:47 +0100

    better DTO test

--- src/test/java/com/example/g6clientproject/JUnitTestDTO.java
+++ src/test/java/com/example/g6clientproject/JUnitTestDTO.java
@@ -1,6 +1,7 @@
 package com.example.g6clientproject;
 
 import  com.example.g6clientproject.DTO.SoftwareDTO;
+import  com.example.g6clientproject.DTO.UserDTO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,6 +16,18 @@ public class JUnitTestDTO {
         assertEquals("MS Office", Software.getSoftware());
         assertEquals("Yes", Software.getAvailability());
         assertEquals("01-01-2024", Software.getRenewal());
+    }
+
+    @Test
+    public void UserDTOTest() {
+        UserDTO user = new UserDTO(1, "admin", "male", 898, "3 sharrold road, CF23 4FD", "dave@bipSync.com", "1990-12-01");
+        assertEquals(1, user.getUserNumber());
+        assertEquals("admin", user.getAccountType());
+        assertEquals("male", user.getGender());
+        assertEquals(898, user.getContactNumber());
+        assertEquals("3 sharrold road, CF23 4FD", user.getAddress());
+        assertEquals("dave@bipSync.com", user.getEmail());
+        assertEquals("1990-12-01", user.getBirthday());
     }
 }
 
