c112eee8d522952afa63312abe0a25852799e253
Qiming <wangq79@cardiff.ac.uk>
2024-04-26 01:28:29 +0100

    test update

---
+++ src/test/java/com/example/g6clientproject/SecurityFullContainerMockMVtests.java
@@ -0,0 +1,39 @@
+package com.example.g6clientproject;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityFullContainerMockMVtests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testNotAddSoftware() throws Exception {
+        this.mockMvc.perform(get("/SoftwareCreate")).andDo(print()).andExpect(status().isFound())
+                .andExpect(header().string("Location", "http://localhost/Home"));
+    }
+
+    @Test
+    public void testNotAddHardware() throws Exception {
+        this.mockMvc.perform(get("/HardwareCreate")).andDo(print()).andExpect(status().isFound())
+                .andExpect(header().string("Location", "http://localhost/Home"));
+    }
+
+    @Test
+    public void testNotAddUser() throws Exception {
+        this.mockMvc.perform(get("/UserInfo")).andDo(print()).andExpect(status().isFound())
+                .andExpect(header().string("Location", "http://localhost/Home"));
+    }
+}
